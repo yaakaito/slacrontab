047fe2c42e639c657de904d96b3a6aacb73506e1
yaakaito <yaakaito@gmail.com>
2019-09-25 04:37:04 +0900

    slack notifier

--- bin/index.ts
+++ bin/index.ts
@@ -1,1 +1,16 @@
-console.log('hello')
+import program from 'commander'
+import axios from 'axios'
+
+program.option('--slack [type]', 'slack incoming hook key')
+program.parse(process.argv)
+
+const main = async() => {
+    try {
+        await axios.post(`https://hooks.slack.com/services/${program.slack}`, JSON.stringify({
+            "text": "hi."
+        }))
+    } catch(e) {
+        console.error(e)
+    }
+}
+main()
