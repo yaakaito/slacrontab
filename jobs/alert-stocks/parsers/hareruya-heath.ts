8738113eff4997e7c4d1784feb21e7ef45051fb1
KAZUMA Ukyo <yaakaito@gmail.com>
2019-12-05 10:08:41 +0900

    Update hareruya-heath.ts

--- jobs/alert-stocks/parsers/hareruya-heath.ts
+++ jobs/alert-stocks/parsers/hareruya-heath.ts
@@ -6,7 +6,7 @@ module.exports = async(url: string = 'https://www.hareruyamtg.com/ja/products/de
     const $ = cheerio.load(response.data)
     const stock = parseInt($('#priceTable-EN > div > div:nth-child(2) > div:nth-child(3)').text(), 10)
     return {
-        id: 'hareruya-wrenn-foil',
+        id: 'hareruya-heath-foil',
         name: 'Windswept Heath(Judge Foil)',
         url,
         stock,
