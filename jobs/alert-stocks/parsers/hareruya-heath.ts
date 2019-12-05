b2e41abe857da1b8ec205a2d9df92f135a1fbb78
KAZUMA Ukyo <yaakaito@gmail.com>
2019-12-05 10:08:33 +0900

    Update and rename hareruya-wrenn.ts to hareruya-heath.ts

--- jobs/alert-stocks/parsers/hareruya-heath.ts
+++ jobs/alert-stocks/parsers/hareruya-heath.ts
@@ -1,13 +1,13 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
 
-module.exports = async(url: string = 'https://www.hareruyamtg.com/ja/products/detail/73766?lang=EN') => {
+module.exports = async(url: string = 'https://www.hareruyamtg.com/ja/products/detail/30235?lang=EN') => {
     const response = await axios.get<string>(url)
     const $ = cheerio.load(response.data)
     const stock = parseInt($('#priceTable-EN > div > div:nth-child(2) > div:nth-child(3)').text(), 10)
     return {
         id: 'hareruya-wrenn-foil',
-        name: 'Wrenn(Foil)',
+        name: 'Windswept Heath(Judge Foil)',
         url,
         stock,
     }
