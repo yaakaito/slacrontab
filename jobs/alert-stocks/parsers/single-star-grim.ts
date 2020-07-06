2f64ea8ae038e31d5320aa2e1ac4718613b9d64f
KAZUMA Ukyo <yaakaito@gmail.com>
2020-07-06 16:48:40 +0900

    Update single-star-grim.ts

--- jobs/alert-stocks/parsers/single-star-grim.ts
+++ jobs/alert-stocks/parsers/single-star-grim.ts
@@ -4,7 +4,7 @@ import cheerio from 'cheerio'
 module.exports = async(url: string = 'https://www.singlestar.jp/product/181099') => {
     const response = await axios.get<string>(url)
     const $ = cheerio.load(response.data)
-    const stock = parseInt($('#purchase_qty option:last-child').value(), 10)
+    const stock = parseInt($('#purchase_qty option:last-child').val(), 10)
     return {
         id: 'single-start-grim',
         name: 'Grim Tutor(Foil showcase)',
