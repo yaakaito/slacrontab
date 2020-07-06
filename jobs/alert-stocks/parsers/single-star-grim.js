5f6c4179e48f18eb032bd8e24bf86d88578de938
KAZUMA Ukyo <yaakaito@gmail.com>
2020-07-06 16:44:41 +0900

    Create single-star-grim.js

---
+++ jobs/alert-stocks/parsers/single-star-grim.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+
+module.exports = async(url: string = 'https://www.singlestar.jp/product/181099') => {
+    const response = await axios.get<string>(url)
+    const $ = cheerio.load(response.data)
+    const stock = parseInt($('#purchase_qty option:last-child').value(), 10)
+    return {
+        id: 'single-start-grim',
+        name: 'Grim Tutor(Foil showcase)',
+        url,
+        stock,
+    }
+}
